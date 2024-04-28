0ff49246525adb8417af7942eb40d7819e7fb608
Ethan Majchrowski <ethanmajchrowski1@gmail.com>
2024-04-28 13:24:00 -0600

    initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       ethan                                                        #
+# 	Created:      4/28/2024, 1:23:04 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
