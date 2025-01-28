112c8973fdf72ba5ee09c22bf84b9fb8b3779f13
Ethan Majchrowski <ethanmajchrowski1@gmail.com>
2025-01-27 19:56:12 -0700

    updated to work with jerry paths with multiple path sections

--- src/unused/path_fixer.py
+++ src/unused/path_fixer.py
@@ -14,20 +14,43 @@ def get_latest_file(download_path):
 with open(get_latest_file(r"C:\Users\ethan\Downloads"), "r") as f:
     data = f.readlines()
 
+#* READ SECTIONS THAT CORRESPOND TO EACH PATH
+all_paths_segments = []
 # Cuts off the first and last lines
-data = data[1:-2] 
-nd = []
-for line in data:
-    line = line.strip().split(",")[:2] # only keep x, y
-    nl = []
-    for i, num in enumerate(line):
-        num = round(float(num)*10, 2)
-        nl.append(num)
+path_segments = [1]
+for i, line in enumerate(data[1:]):
+    if "#PATH-" in line:
+        # we have found the start of the next path
+        # finish the previous path and start the new one
+        # finish the previous path
+        path_segments.append(i)
+        all_paths_segments.append(path_segments)
+        path_segments = [i+2]
+    if "PATH." in line:
+        # last marker, tie off last path
+        path_segments.append(i)
+        all_paths_segments.append(path_segments)
+print(all_paths_segments)
 
-    nd.append(tuple(nl))
+# # get data from line with #PATH-POINTS-START - #PATH-POINTS-START and #PATH-POINTS-START - #PATH.JERRYIO-DATA
+#* SPLIT BIG DATA INTO EACH PATH
+for path_number, path in enumerate(all_paths_segments):
+    # print(path)
+    this_path_data = data[path[0]:path[1]]
+    nd = []
+    for line in this_path_data:
+        line = line.strip().split(",")[:2] # only keep x, y
+        nl = []
+        for i, num in enumerate(line):
+            num = round(float(num)*10, 2)
+            nl.append(num)
 
-path = tuple(nd)
+        nd.append(tuple(nl))
 
-print(path)
+    path = tuple(nd)
+    print(f"Path {path_number+1}")
+    print(path, end=",\n")
+
+# print(path)
 # pyperclip.copy(str(path))
 # print("Copied to clipboard!")
