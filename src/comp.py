d97d21fb40504a373cb6484b172947a05752fff6
Ethan Majchrowski <ethanmajchrowski1@gmail.com>
2025-02-10 08:41:59 -0700

    moved to SD card

--- src/comp.py
+++ src/comp.py
@@ -566,97 +566,14 @@ class AutonomousHandler:
         """
         Run test autons. Does NOT! run in competition
         """
-        self.setup_autonomous()
-
-        self.dynamic_vars["position"] = [1560, 330]
-        self.heading = 150
-        imu.set_heading(150)
-        imu.set_rotation(150)
-
-        paths = [((1547.38, 312.87), (1509.88, 405.33), (1452.3, 486.82), (1380.58, 556.21), (1298.65, 613.28), (1208.81, 656.95), (1113.23, 685.8), (1014.32, 699.16), (914.61, 694.22), (818.23, 668.6), (729.22, 623.74), (652.36, 560.13), (591.56, 488.41)), ((527.49, 491.49), (529.01, 591.48), (532.83, 691.34), (542.3, 790.89), (560.04, 889.23), (587.01, 985.33), (627.9, 1076.22), (689.61, 1154.0), (778.34, 1197.53), (876.59, 1189.84), (964.99, 1144.69), (1038.62, 1077.42), (1099.53, 998.31), (1146.37, 910.09), (1180.7, 816.33), (1202.17, 718.85), (1209.89, 619.35), (1200.2, 496.18)), ((1218.5, 496.18), (1211.46, 396.42), (1206.02, 296.57), (1203.31, 196.62), (1203.66, 96.62), (1204.54, -3.38), (1203.67, -103.37), (1200.95, -203.33), (1195.62, -330.73)), ((1204.77, -355.0), (1147.2, -275.02), (1156.64, -176.84), (1185.02, -80.96), (1203.69, 17.2), (1209.35, 98.04)), ((1349.18, 94.91), (1257.78, 54.48), (1172.02, 3.31), (1096.09, -61.32), (1036.3, -141.17), (999.44, -233.89), (984.97, -332.65), (987.87, -432.49), (1002.22, -531.4), (1023.46, -629.1), (1048.39, -725.94), (1073.97, -822.61), (1089.5, -918.83), (1045.74, -1008.74), (997.21, -1096.14), (940.24, -1178.24), (873.06, -1252.15), (795.34, -1314.83), (708.61, -1364.27), (615.53, -1400.47), (518.49, -1424.36), (419.45, -1437.68), (280.37, -1442.76)), ((377.5, -1376.02), (433.93, -1293.46), (489.34, -1210.22), (542.1, -1125.28), (590.03, -1037.54), (630.38, -946.1), (659.69, -850.59), (674.18, -751.77), (672.92, -651.91), (656.66, -554.66)), ((640.55, -640.55), (628.55, -541.28), (621.72, -441.54), (623.2, -341.59), (634.12, -242.22), (647.45, -143.12), (653.03, -43.32), (646.47, 102.62))]
-
-        main = globals()
-        controller = self
-
-        main["LB_enable_PID"] = False
-        motors["misc"]["wall_stake"].stop(BrakeType.COAST)
-        # controller.heading = 150 # 150!
-        main["sleep"](100, TimeUnits.MSEC)
-        # controller.dynamic_vars["position"] = [1560, 330]
-        # controller.dynamic_vars["intake_color_sort"] = "eject_red"
-
-        # drop preload onto alliance wall stake
-        # motors["misc"]["wall_stake"].spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
-        # wait for LB to move
+        self.run()
+        # self.setup_autonomous()
         
-        print(brain.timer.system())
-        end_time = 7500 + brain.timer.system()
-        print(end_time)
 
-        main["wallEnc"].set_position(20)
-        while (main["wallEnc"].position() < 380):
-            print(main["wallEnc"].position())
-            if (brain.timer.system() > end_time):
-                break
-            else:
-                motors["misc"]["wall_stake"].spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
-            main["sleep"](20)
+        # main = globals()
+        # controller = self
 
-        print(main["wallEnc"].position())
-        # stop LB mech
-        motors["misc"]["wall_stake"].stop(BrakeType.COAST)
-        # path to grab mogo @ 8V
-        controller.fwd_speed = 7 * 1.15
-        brain.timer.event(motors["misc"]["wall_stake"].spin, 200, (DirectionType.REVERSE, 60, VelocityUnits.PERCENT))
-        controller.path(paths[0], [], [], True, 200, 100, 75, None, 1.25, 8, 0.1, 0.01, 0, 0, None, 0)
-        motors["misc"]["wall_stake"].stop(BrakeType.COAST)
-        # after we are at the mogo start retracting the LB bar
-        # grab mobile goal
-        main["mogo_pneu"].set(True)
-        # start intaking rings
-        brain.timer.event(motors["misc"]["intake_chain"].spin, 200, (DirectionType.FORWARD, 100, VelocityUnits.PERCENT))
-        motors["misc"]["intake_flex"].spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
-        # wait for mogo grabber to engage
-        main["sleep"](200, TimeUnits.MSEC)
-
-        controller.fwd_speed = 8
-        controller.path(paths[1], [], [], False, 325, 100, 75, None, 2.5)
-        main["intake_pneu"].set(True)
-        # motors["misc"]["intake_chain"].stop()
-        controller.path(paths[2], [], [], False, 350, 160)
-        motors["misc"]["intake_chain"].spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
-
-        main["intake_pneu"].set(False)
-        controller.fwd_speed = 5
-        sleep(500, MSEC) # sleep to allow for top stack ring to get scored
-        print("backup")
-        controller.path(paths[3], [], [], True)
-        sleep(500, MSEC) # sleep to allow for top stack ring to get scored
-
-        controller.fwd_speed = 7
-        brain.timer.event(motors["misc"]["intake_chain"].stop, 200)
-        # motors["misc"]["intake_flex"].stop()
-        # controller.dynamic_vars["intake_auto_halt"] = True
-        # controller.dynamic_vars["drop_after_auto_halt"] = True
-        print("path: bottom_blue_ring")
-        controller.path(paths[4], [["drop goal", (951, -1162), main["mogo_pneu"].set, [False]]], [], False, 250, 150, 200, None, 2.0)
-
-        main["mogo_pneu"].set(False)
-
-        print("path: last_mogo")
-        controller.fwd_speed = 6 * 1.15
-        controller.path(paths[5], [], [], True, 300, 175, 75, None, 2)
-        main["mogo_pneu"].set(True)
-        motors["misc"]["intake_flex"].spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
-
-        # controller.dynamic_vars["intake_color_sort"] = "none" # this might be needed, idk
         
-        drivetrain.turn_for(TurnType.LEFT, 150, RotationUnits.DEG, 100, VelocityUnits.PERCENT)
-        motors["misc"]["intake_chain"].spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
-        sleep(500, MSEC)
-        controller.fwd_speed = 10
-        print("path: ladder_hit")
-        controller.path(paths[6], [], [], False, 300, 70, 75, 2000, 2)
     
     def setup_autonomous(self):
         """
