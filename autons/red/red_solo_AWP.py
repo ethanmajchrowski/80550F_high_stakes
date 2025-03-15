174c55b5340b611bf2c0bd8edbe2519fe1618ae2
Ethan Majchrowski <ethanmajchrowski1@gmail.com>
2025-03-14 21:40:16 -0600

    tuned to be better

--- autons/red/red_solo_AWP.py
+++ autons/red/red_solo_AWP.py
@@ -32,80 +32,50 @@ def run(main):
     ColorSort = main["ColorSort"]
     sleep = main["sleep"]
     #endregion Bindings
-    paths = [((-1547.38, 312.87), (-1509.84, 405.3), (-1449.82, 485.02), (-1373.22, 549.05), (-1285.75, 597.23), (-1191.58, 630.47), (-1093.57, 649.61), (-993.85, 654.94), (-894.34, 646.14), (-797.41, 621.95), (-706.28, 581.18), (-651.29, 545.86)),((-576.13, 492.17), (-523.6, 577.15), (-483.29, 668.58), (-453.25, 763.91), (-431.37, 861.45), (-415.67, 960.19), (-404.4, 1059.55), (-395.83, 1159.18), (-388.09, 1258.88), (-378.5, 1358.41), (-461.27, 1402.91), (-559.22, 1418.49), (-645.27, 1374.28), (-687.09, 1284.25), (-714.08, 1188.01), (-795.5, 1156.71), (-895.37, 1161.49), (-995.24, 1157.71), (-1093.89, 1142.3), (-1187.11, 1107.14), (-1264.9, 1045.35), (-1316.95, 960.57), (-1344.49, 864.61), (-1356.97, 765.45), (-1360.0, 665.52), (-1358.19, 565.54), (-1352.15, 465.74), (-1341.44, 366.33), (-1327.26, 267.34), (-1311.34, 168.61), (-1295.76, 69.84), (-1283.91, -29.44), (-1282.79, -129.31), (-1305.12, -226.18), (-1365.06, -304.81), (-1413.57, -336.74)),((-1446.26, -344.01), (-1378.21, -271.14), (-1346.34, -177.62), (-1346.82, -77.7), (-1352.03, 22.16), (-1354.55, 100.28)),((-1349.18, 94.91), (-1303.19, 6.14), (-1264.05, -85.86), (-1231.49, -180.39), (-1205.05, -276.81), (-1184.06, -374.57), (-1167.77, -473.23), (-1155.52, -572.47), (-1146.6, -672.06), (-1140.29, -771.86), (-1136.03, -871.77), (-1114.25, -967.15), (-1062.54, -1052.65), (-998.28, -1129.13), (-922.81, -1194.55), (-838.33, -1247.85), (-747.2, -1288.82), (-651.96, -1319.13), (-554.36, -1340.78)),((-377.5, -1376.02), (-433.93, -1293.46), (-489.34, -1210.22), (-542.1, -1125.28), (-590.03, -1037.54), (-630.38, -946.1), (-659.69, -850.59), (-674.18, -751.77), (-672.92, -651.91), (-656.66, -554.66)),((-640.55, -640.55), (-631.43, -541.01), (-633.9, -441.14), (-652.46, -343.03), (-686.34, -249.04), (-726.18, -157.33), (-757.44, -62.47), (-771.64, 36.36), (-769.39, 137.86)),]
+    paths = [[(-1547.38, 312.87, 0), (-1521.81, 399.14, 0.17), (-1489.72, 483.18, 0.19), (-1450.73, 564.24, 0.25), (-1403.02, 640.48, 0.31), (-1345.13, 709.18, 0.39), (-1276.14, 766.61, 0.53), (-1195.49, 805.97, 0.6), (-1107.42, 822.31, 0.52), (-1017.82, 817.44, 0.49), (-931.39, 792.98, 0.34), (-849.59, 755.75, 0.25), (-772.43, 709.61, 0.19), (-699.42, 657.11, 0.14), (-629.82, 600.06, 0.14), (-564.03, 538.65, 0.09), (-500.91, 474.55, 0.05)], [(-576.13, 492.17, 0), (-520.44, 562.85, 0.09), (-462.02, 631.31, 0.14), (-408.01, 703.2, 0.45), (-369.7, 784.41, 0.42), (-347.51, 871.55, 0.24), (-334.65, 960.59, 0.13), (-326.96, 1050.25, 0.08), (-322.37, 1140.13, 0.05), (-319.65, 1230.08, 0.03), (-318.28, 1320.07, 1.03), (-355.14, 1397.73, 0.68), (-416.54, 1463.36, 0.8), (-495.52, 1502.5, 2.01), (-567.26, 1455.31, 1.12), (-607.39, 1374.93, 0.19), (-640.67, 1291.31, 0.26), (-683.34, 1212.43, 1.63), (-763.18, 1185.94, 0.92), (-852.66, 1195.6, 0.01), (-942.19, 1204.86, 0.02), (-1031.78, 1213.34, 0.04), (-1121.51, 1220.19, 0.09), (-1211.42, 1223.58, 0.18), (-1301.27, 1219.82, 0.54), (-1387.13, 1194.64, 1.32), (-1441.34, 1125.86, 1.13), (-1453.38, 1037.18, 0.42), (-1448.44, 947.36, 0.13), (-1438.11, 857.97, 0.05), (-1425.77, 768.82, 0.01), (-1413.1, 679.72, 0.01), (-1400.86, 590.55, 0.02), (-1389.32, 501.3, 0.02), (-1378.58, 411.94, 0.04), (-1369.37, 322.42, 0.02), (-1360.9, 232.82, 0.03), (-1353.81, 143.1, 0.03), (-1347.85, 53.31, 0.03), (-1342.91, -36.56, 0.03), (-1339.28, -126.47, 0.1), (-1339.89, -216.47, 0.02), (-1339.63, -306.46, 0.06), (-1336.85, -396.41, 0.06), (-1331.79, -486.25, 0.05), (-1324.74, -575.96, 0.11), (-1313.32, -665.24, 0.1), (-1297.99, -753.89, 0.12), (-1277.83, -841.54, 0.16), (-1251.33, -927.46, 0.21), (-1216.71, -1010.42, 0.27), (-1172.23, -1088.5, 0.37), (-1115.43, -1158.03, 0.44), (-1046.05, -1215.04, 0.45), (-966.65, -1256.88, 0.38), (-881.13, -1284.58, 0.31), (-792.52, -1300.04, 0.17), (-702.96, -1308.74, 0.1), (-613.09, -1313.55, 0.01), (-523.21, -1318.1, 0.07)], [(-369.79, -1307.3, 0), (-425.31, -1236.53, 0.3), (-489.87, -1173.93, 0.23), (-560.63, -1118.36, 0.0), (-631.3, -1062.69, 0.37), (-691.5, -996.14, 0.55), (-733.52, -916.81, 0.44), (-759.04, -830.6, 0.29), (-773.23, -741.76, 0.16), (-780.96, -652.12, 0.12)], [(-640.55, -640.55, 0), (-631.85, -551.0, 0.22), (-632.24, -461.08, 0.31), (-645.01, -372.1, 0.34), (-671.18, -286.07, 0.24), (-706.44, -203.28, 0.03), (-740.54, -120.01, 0.28), (-763.83, -33.18, 0.37), (-772.2, 56.32, 0.31)]]
+    sensor.imu.set_heading(210)
+    controller.robot.pos = [-1560.0, 330.0]
+    
+    # ladder touch
+    # doinker
 
     flags.color_setting = ColorSort.EJECT_BLUE
-    
-    end_time = 7500 + brain.timer.system()
-    log("Spinning lady brown from {} to {} or until threshold.".format(brain.timer.system(), end_time))
+    main["AutonomousFlags"].intake_auto_stop_type = BrakeType.COAST
+    motor.ladyBrown.spin(DirectionType.FORWARD, 80, VelocityUnits.PERCENT)
+    sleep(400, TimeUnits.MSEC)
 
-    sensor.wallEncoder.set_position(20)
-    while (sensor.wallEncoder.position() < 380):
-        if (brain.timer.system() > end_time):
-            break
-        else:
-            motor.ladyBrown.spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
-        sleep(20)
-
-    log("Wall encoder at {}".format(sensor.wallEncoder.position()))
-
-    # stop LB mech
-    motor.ladyBrown.stop(BrakeType.COAST)
-    # path to grab mogo @ 8V
     controller.fwd_speed = 7
-    brain.timer.event(motor.ladyBrown.spin, 200, (DirectionType.REVERSE, 100, VelocityUnits.PERCENT))
-    controller.path(paths[0], [], [], True, 200, 100, 75, None, 1.25, 8, 0.1, 0.01, 0, 0, None, 0)
+    brain.timer.event(motor.ladyBrown.spin, 800, (DirectionType.REVERSE, 80, VelocityUnits.PERCENT))
+    controller.path(paths[0], backwards=True, speed_ramp_time=400, slowdown_distance=600)
     motor.ladyBrown.stop(BrakeType.COAST)
-    # after we are at the mogo start retracting the LB bar
-    # grab mobile goal
     pneumatic.mogo.set(True)
-    # start intaking rings
-    motor.intakeChain.spin(DirectionType.FORWARD, 65, VelocityUnits.PERCENT)
+    drivetrain.turn_for(TurnType.RIGHT, 50, RotationUnits.DEG, 100, VelocityUnits.PERCENT)
+
+    motor.intakeChain.spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
     motor.intakeFlex.spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
-    # wait for mogo grabber to engage
-    sleep(200, TimeUnits.MSEC)
-    # turn to face the rings on the auton line (w/timeout & threshold)
-    drivetrain.set_timeout(1.5, TimeUnits.SECONDS)
-    drivetrain.set_turn_threshold(5)
-    drivetrain.set_turn_constant(0.6)
-    drivetrain.turn_for(TurnType.RIGHT, 90, RotationUnits.DEG, 100, VelocityUnits.PERCENT)
+    controller.fwd_speed = 5
+    controller.path(paths[1], speed_ramp_time=400, heading_authority=1.7, event_look_ahead_dist=200, 
+    events=[
+        ["intake", (-1200.0, 825.0), pneumatic.intake.set, (True,)],
+        ["drop", (-1190.0, -840.0), pneumatic.mogo.set, (False,)],
+        ["turn off color sort", (-1190.0, -840.0), "flags", "color_setting", ColorSort.NONE],
+        ["lower", (-1200.0, -500.0), pneumatic.intake.set, (False,)],
+        ["autostop for last ring", (-1000.0, -1100.0), "AutonomousFlags", "intake_auto_halt", True],
+        ["speed to launch", (-1240.0, 1146.0), "fwd_speed", 7],
+        ["slow for stack", (-1400.0, 644.9), "fwd_speed", 4.5],
+    ],
+    checkpoints=[12,],
+    )
 
-    controller.fwd_speed = 6
-    log("path: grab_border_rings")
-    controller.path(paths[1], [
-        ["raise intake", (-1294, 741), pneumatic.intake.set, (True,)], 
-        ["speed up", (-1294, 741), "fwd_speed", 7]
-    ], [22], False, 200, 130, 120, None, 1.25, 8, 0.1, 0.01, 0, 0, None, 0)
-
-    pneumatic.intake.set(False)
-    sleep(250, TimeUnits.MSEC)
-    controller.fwd_speed = 3
-    log("path: backup_for_stack_ring")
-    controller.path(paths[2], [], [], True)
-    # sleep(200, MSEC)
-
-    motor.intakeFlex.spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
-
-    controller.fwd_speed = 9
-    log("path: bottom_blue_ring")
-    controller.path(paths[3], [["drop goal", (-951, -1162), pneumatic.mogo.set, [False]]], [], False, 300, 100, 200)
-
-    motor.intakeChain.stop()
-    pneumatic.mogo.set(False)
-
-    log("path: last_mogo")
-    controller.fwd_speed = 6
-    controller.path(paths[4], [], [], True, 300, 175, 75)
+    controller.fwd_speed = 7
+    controller.path(paths[2], backwards=True, speed_ramp_time=400)
     pneumatic.mogo.set(True)
-    motor.intakeFlex.spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
 
-    flags.color_setting = ColorSort.NONE
-    
-    drivetrain.turn_for(TurnType.RIGHT, 150, RotationUnits.DEG, 100, VelocityUnits.PERCENT)
-    motor.intakeChain.spin(DirectionType.FORWARD, 65, VelocityUnits.PERCENT)
-    controller.fwd_speed = 8
-    log("path: ladder_hit")
-    controller.path(paths[5], [], [], False, 300, 70, 75, 2000)
+    sleep(150, TimeUnits.MSEC)
+    motor.intakeChain.spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
+    drivetrain.turn_for(TurnType.RIGHT, 160, RotationUnits.DEG, 80, VelocityUnits.PERCENT)
+    controller.fwd_speed = 5.5
+    controller.path(paths[3], speed_ramp_time=400, timeout=800)
 
 
 
--- src/comp.py
+++ src/comp.py
@@ -1017,54 +1017,55 @@ class Autonomous():
         Run a test version of autonomous. This is NOT run in competition!
         """
         log("Running autonomous TEST", LogLevel.WARNING)
-        self.run()
-        # self.autonomous_setup()
+        # self.run()
+        self.autonomous_setup()
 
-        # paths = [[(-1547.38, 312.87, 0), (-1521.81, 399.14, 0.17), (-1489.72, 483.18, 0.19), (-1450.73, 564.24, 0.25), (-1403.02, 640.48, 0.31), (-1345.13, 709.18, 0.39), (-1276.14, 766.61, 0.53), (-1195.49, 805.97, 0.6), (-1107.42, 822.31, 0.52), (-1017.82, 817.44, 0.49), (-931.39, 792.98, 0.34), (-849.59, 755.75, 0.25), (-772.43, 709.61, 0.19), (-699.42, 657.11, 0.14), (-629.82, 600.06, 0.14), (-564.03, 538.65, 0.09), (-500.91, 474.55, 0.05)], [(-576.13, 492.17, 0), (-519.38, 561.98, 0.18), (-457.26, 627.08, 0.12), (-398.82, 695.32, 0.6), (-360.99, 776.59, 0.49), (-341.78, 864.43, 0.22), (-331.39, 953.8, 0.11), (-325.36, 1043.59, 0.07), (-322.06, 1133.53, 0.04), (-320.25, 1223.51, 0.47), (-335.94, 1308.08, 1.05), (-392.22, 1378.26, 0.42), (-460.32, 1436.52, 1.45), (-544.65, 1440.57, 1.85), (-598.85, 1370.02, 0.49), (-636.61, 1288.33, 0.21), (-681.77, 1210.83, 1.6), (-762.78, 1185.9, 0.88), (-852.26, 1195.55, 0.01), (-941.79, 1204.81, 0.02), (-1031.38, 1213.31, 0.04), (-1121.11, 1220.17, 0.09), (-1211.02, 1223.57, 0.18), (-1300.87, 1219.85, 0.54), (-1386.76, 1194.8, 1.32), (-1441.23, 1126.25, 1.14), (-1453.39, 1037.58, 0.42), (-1448.48, 947.76, 0.13), (-1438.16, 858.37, 0.05), (-1425.83, 769.22, 0.01), (-1413.16, 680.12, 0.01), (-1400.92, 590.95, 0.02), (-1389.37, 501.7, 0.02), (-1378.63, 412.34, 0.04), (-1369.41, 322.82, 0.02), (-1360.93, 233.22, 0.03), (-1353.84, 143.5, 0.03), (-1347.87, 53.71, 0.03), (-1342.93, -36.16, 0.03), (-1339.25, -126.07, 0.1), (-1339.61, -216.07, 0.02), (-1339.01, -306.05, 0.07), (-1335.48, -395.98, 0.06), (-1329.47, -485.75, 0.05), (-1321.35, -575.38, 0.13), (-1308.18, -664.41, 0.09), (-1291.23, -752.76, 0.12), (-1269.49, -840.03, 0.16), (-1241.67, -925.54, 0.2), (-1206.32, -1008.22, 0.24), (-1162.12, -1086.48, 0.29), (-1108.17, -1158.33, 0.32), (-1044.41, -1221.65, 0.4), (-970.29, -1272.46, 0.33), (-889.49, -1311.82, 0.31), (-803.95, -1339.48, 0.23), (-715.92, -1358.02, 0.16), (-626.75, -1369.99, 0.1), (-537.09, -1377.81, 0.03), (-447.32, -1384.26, 0.02)], [(-377.5, -1376.02, 0), (-432.04, -1304.46, 0.22), (-493.34, -1238.62, 0.14), (-558.77, -1176.83, 0.03), (-623.32, -1114.13, 0.28), (-679.53, -1044.02, 0.4), (-722.07, -964.88, 0.39), (-750.12, -879.44, 0.27), (-767.59, -791.19, 0.19), (-777.58, -701.77, 0.13), (-782.52, -611.92, 0.12)], [(-640.55, -640.55, 0), (-631.85, -551.0, 0.22), (-632.24, -461.08, 0.31), (-645.01, -372.1, 0.34), (-671.18, -286.07, 0.24), (-706.44, -203.28, 0.03), (-740.54, -120.01, 0.28), (-763.83, -33.18, 0.37), (-772.2, 56.32, 0.31)]]
-        # sensor.imu.set_heading(210)
-        # self.robot.pos = [-1560.0, 330.0]
+        paths = [[(-1547.38, 312.87, 0), (-1521.81, 399.14, 0.17), (-1489.72, 483.18, 0.19), (-1450.73, 564.24, 0.25), (-1403.02, 640.48, 0.31), (-1345.13, 709.18, 0.39), (-1276.14, 766.61, 0.53), (-1195.49, 805.97, 0.6), (-1107.42, 822.31, 0.52), (-1017.82, 817.44, 0.49), (-931.39, 792.98, 0.34), (-849.59, 755.75, 0.25), (-772.43, 709.61, 0.19), (-699.42, 657.11, 0.14), (-629.82, 600.06, 0.14), (-564.03, 538.65, 0.09), (-500.91, 474.55, 0.05)], [(-576.13, 492.17, 0), (-520.44, 562.85, 0.09), (-462.02, 631.31, 0.14), (-408.01, 703.2, 0.45), (-369.7, 784.41, 0.42), (-347.51, 871.55, 0.24), (-334.65, 960.59, 0.13), (-326.96, 1050.25, 0.08), (-322.37, 1140.13, 0.05), (-319.65, 1230.08, 0.03), (-318.28, 1320.07, 1.03), (-355.14, 1397.73, 0.68), (-416.54, 1463.36, 0.8), (-495.52, 1502.5, 2.01), (-567.26, 1455.31, 1.12), (-607.39, 1374.93, 0.19), (-640.67, 1291.31, 0.26), (-683.34, 1212.43, 1.63), (-763.18, 1185.94, 0.92), (-852.66, 1195.6, 0.01), (-942.19, 1204.86, 0.02), (-1031.78, 1213.34, 0.04), (-1121.51, 1220.19, 0.09), (-1211.42, 1223.58, 0.18), (-1301.27, 1219.82, 0.54), (-1387.13, 1194.64, 1.32), (-1441.34, 1125.86, 1.13), (-1453.38, 1037.18, 0.42), (-1448.44, 947.36, 0.13), (-1438.11, 857.97, 0.05), (-1425.77, 768.82, 0.01), (-1413.1, 679.72, 0.01), (-1400.86, 590.55, 0.02), (-1389.32, 501.3, 0.02), (-1378.58, 411.94, 0.04), (-1369.37, 322.42, 0.02), (-1360.9, 232.82, 0.03), (-1353.81, 143.1, 0.03), (-1347.85, 53.31, 0.03), (-1342.91, -36.56, 0.03), (-1339.28, -126.47, 0.1), (-1339.89, -216.47, 0.02), (-1339.63, -306.46, 0.06), (-1336.85, -396.41, 0.06), (-1331.79, -486.25, 0.05), (-1324.74, -575.96, 0.11), (-1313.32, -665.24, 0.1), (-1297.99, -753.89, 0.12), (-1277.83, -841.54, 0.16), (-1251.33, -927.46, 0.21), (-1216.71, -1010.42, 0.27), (-1172.23, -1088.5, 0.37), (-1115.43, -1158.03, 0.44), (-1046.05, -1215.04, 0.45), (-966.65, -1256.88, 0.38), (-881.13, -1284.58, 0.31), (-792.52, -1300.04, 0.17), (-702.96, -1308.74, 0.1), (-613.09, -1313.55, 0.01), (-523.21, -1318.1, 0.07)], [(-369.79, -1307.3, 0), (-425.31, -1236.53, 0.3), (-489.87, -1173.93, 0.23), (-560.63, -1118.36, 0.0), (-631.3, -1062.69, 0.37), (-691.5, -996.14, 0.55), (-733.52, -916.81, 0.44), (-759.04, -830.6, 0.29), (-773.23, -741.76, 0.16), (-780.96, -652.12, 0.12)], [(-640.55, -640.55, 0), (-631.85, -551.0, 0.22), (-632.24, -461.08, 0.31), (-645.01, -372.1, 0.34), (-671.18, -286.07, 0.24), (-706.44, -203.28, 0.03), (-740.54, -120.01, 0.28), (-763.83, -33.18, 0.37), (-772.2, 56.32, 0.31)]]
+        sensor.imu.set_heading(210)
+        self.robot.pos = [-1560.0, 330.0]
         
-        # # ladder touch
-        # # doinker
+        # ladder touch
+        # doinker
 
-        # flags.color_setting = ColorSort.EJECT_BLUE
-        # motor.ladyBrown.spin(DirectionType.FORWARD, 80, VelocityUnits.PERCENT)
-        # sleep(400, TimeUnits.MSEC)
+        flags.color_setting = ColorSort.EJECT_BLUE
+        AutonomousFlags.intake_auto_stop_type = BrakeType.COAST
+        motor.ladyBrown.spin(DirectionType.FORWARD, 80, VelocityUnits.PERCENT)
+        sleep(400, TimeUnits.MSEC)
 
-        # self.fwd_speed = 7
-        # brain.timer.event(motor.ladyBrown.spin, 800, (DirectionType.REVERSE, 80, VelocityUnits.PERCENT))
-        # self.path(paths[0], backwards=True, speed_ramp_time=400, slowdown_distance=600)
-        # motor.ladyBrown.stop(BrakeType.COAST)
-        # pneumatic.mogo.set(True)
-        # drivetrain.turn_for(TurnType.RIGHT, 50, RotationUnits.DEG, 100, VelocityUnits.PERCENT)
+        self.fwd_speed = 7
+        brain.timer.event(motor.ladyBrown.spin, 800, (DirectionType.REVERSE, 80, VelocityUnits.PERCENT))
+        self.path(paths[0], backwards=True, speed_ramp_time=400, slowdown_distance=600)
+        motor.ladyBrown.stop(BrakeType.COAST)
+        pneumatic.mogo.set(True)
+        drivetrain.turn_for(TurnType.RIGHT, 50, RotationUnits.DEG, 100, VelocityUnits.PERCENT)
 
-        # motor.intakeChain.spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
-        # motor.intakeFlex.spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
-        # self.fwd_speed = 5
-        # self.path(paths[1], speed_ramp_time=400, heading_authority=1.7, event_look_ahead_dist=200, 
-        # events=[
-        #     ["intake", (-1200.0, 825.0), pneumatic.intake.set, (True,)],
-        #     ["drop", (-1190.0, -840.0), pneumatic.mogo.set, (False,)],
-        #     ["turn off color sort", (-1190.0, -840.0), "flags", "color_setting", ColorSort.NONE],
-        #     ["lower", (-1200.0, -500.0), pneumatic.intake.set, (False,)],
-        #     ["autostop for last ring", (-1000.0, -1100.0), "AutonomousFlags", "intake_auto_halt", True],
-        #     ["speed to launch", (-1240.0, 1146.0), "fwd_speed", 7],
-        #     ["slow for stack", (-1400.0, 644.9), "fwd_speed", 4.5],
-        # ],
-        # checkpoints=[12,],
-        # )
+        motor.intakeChain.spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
+        motor.intakeFlex.spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
+        self.fwd_speed = 5
+        self.path(paths[1], speed_ramp_time=400, heading_authority=1.7, event_look_ahead_dist=200, 
+        events=[
+            ["intake", (-1200.0, 825.0), pneumatic.intake.set, (True,)],
+            ["drop", (-1190.0, -840.0), pneumatic.mogo.set, (False,)],
+            ["turn off color sort", (-1190.0, -840.0), "flags", "color_setting", ColorSort.NONE],
+            ["lower", (-1200.0, -500.0), pneumatic.intake.set, (False,)],
+            ["autostop for last ring", (-1000.0, -1100.0), "AutonomousFlags", "intake_auto_halt", True],
+            ["speed to launch", (-1240.0, 1146.0), "fwd_speed", 7],
+            ["slow for stack", (-1400.0, 644.9), "fwd_speed", 4.5],
+        ],
+        checkpoints=[12,],
+        )
 
-        # self.fwd_speed = 7
-        # self.path(paths[2], backwards=True, speed_ramp_time=400)
-        # pneumatic.mogo.set(True)
+        self.fwd_speed = 7
+        self.path(paths[2], backwards=True, speed_ramp_time=400)
+        pneumatic.mogo.set(True)
 
-        # sleep(150, TimeUnits.MSEC)
-        # motor.intakeChain.spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
-        # drivetrain.turn_for(TurnType.RIGHT, 160, RotationUnits.DEG, 80, VelocityUnits.PERCENT)
-        # self.fwd_speed = 5.5
-        # self.path(paths[3], speed_ramp_time=400, timeout=800)
+        sleep(150, TimeUnits.MSEC)
+        motor.intakeChain.spin(DirectionType.FORWARD, 100, VelocityUnits.PERCENT)
+        drivetrain.turn_for(TurnType.RIGHT, 160, RotationUnits.DEG, 80, VelocityUnits.PERCENT)
+        self.fwd_speed = 5.5
+        self.path(paths[3], speed_ramp_time=400, timeout=800)
 
-        # self.autonomous_cleanup()
+        self.autonomous_cleanup()
     
     def background(self) -> None:
         """
